b7d2df6dc03450114ebcd6c36410500e975c5529
Hcw <156618992@qq.com>
2017-12-29 12:51:34 +0800

    12月29日 单机版和网络版的坦克

---
+++ TankDemoAiAndNet/Client/Assets/PanelCode/ChoosePanel.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChoosePanel : PanelBase
+{
+    public static int type;
+    private Button aiFight;
+    private Button netFight;
+    private Button infoBtn;
+    private Button closeBtn;
+
+    #region 生命周期
+    public override void Init(params object[] args)
+    {
+        base.Init(args);
+        skinPath = "ChoosePanel";
+        layer = PanelLayer.Panel;
+    }
+
+    public override void OnShowing()
+    {
+        base.OnShowing();
+        Transform skinTrans = skin.transform;
+        aiFight = skinTrans.FindChild("AI").GetComponent<Button>();
+        netFight = skinTrans.FindChild("Net").GetComponent<Button>();
+        infoBtn = skinTrans.FindChild("infoBtn").GetComponent<Button>();
+        closeBtn = skinTrans.FindChild("CloseBtn").GetComponent<Button>();
+        //添加事件
+        aiFight.onClick.AddListener(OnAiClick);
+        netFight.onClick.AddListener(OnNetClick);
+        infoBtn.onClick.AddListener(OnInfoClick);
+        closeBtn.onClick.AddListener(OnEndGame);
+    }
+    #endregion
+
+
+    public void OnAiClick()
+    {
+        type = 0;
+        //设置
+        PanelMgr.instance.OpenPanel<OptionPanel>("");
+        PanelMgr.instance.ClearPanels();
+    }
+
+    public void OnInfoClick()
+    {
+        PanelMgr.instance.OpenPanel<InfoPanel>("");
+    }
+
+    public void OnNetClick()
+    {
+        type = 1;
+        PanelMgr.instance.OpenPanel<LoginPanel>("");
+        Close();
+    }
+    public void OnEndGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ TankDemoAiAndNet/Client/Assets/core/Root.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Root : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Application.runInBackground = true;
+        PanelMgr.instance.OpenPanel<ChoosePanel>("");
+	}
+
+    void Update()
+    {
+        NetMgr.Update();
+    }
+}
